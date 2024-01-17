2016a33ca28c8595d66877fc95c4a4f805fb0c89
Ben Klimchock <klimmy1115@gmail.com>
2024-01-17 09:17:29 -0600

    Commit from VS Code

--- Role Playing Game/app.js
+++ Role Playing Game/app.js
@@ -118,6 +118,8 @@ function sellWeapon() {
         currentWeapon = inventory.shift();
         text.innerText = `You sold a ${currentWeapon}.`;
         text.innerText += ` In your inventory you have: ${inventory}`;
+    } else {
+        text.innerText = "You can't sell your only weapon!";
     }
 }
 
