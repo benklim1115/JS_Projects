62d1432402219af265525d794f8e449ed8c361e0
Ben Klimchock <klimmy1115@gmail.com>
2024-01-26 08:57:48 -0600

    function works but lets use another method

--- Calorie Counter/app.js
+++ Calorie Counter/app.js
@@ -4,3 +4,28 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false;
+
+
+//convert user input to number 
+function cleanInputString(str) {
+    //use regex instead to help with memory and runtime performance
+    const regex = /hello/;
+    /*
+    const strArr = str.split('');
+    const cleanStrArr = [];
+
+    //check for characters we don't want
+
+    const badValues = ["+", "-", " "];
+    for (let i = 0; i < strArr.length; i++) {
+        if (!badValues.includes(strArr[i])) {
+            cleanStrArr.push(strArr[i]);
+        } 
+    }
+
+    const joinedArr = cleanStrArr.join("");
+
+    return joinedArr;
+    */
+}
