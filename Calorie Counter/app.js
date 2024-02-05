8d797a52aae13b2da8e176d58e3c32b9d6498b3a
Ben Klimchock <klimmy1115@gmail.com>
2024-02-05 08:45:09 -0600

    All our inputs

--- Calorie Counter/app.js
+++ Calorie Counter/app.js
@@ -72,6 +72,11 @@ function calculateCalories(e) {
     preventDefault(e);
     isError = false;
 
-    let breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    //track all of our inputs to be calculated
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 }
 
