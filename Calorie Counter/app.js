30268450991c8568ca82a997876c7df496b07689
Ben Klimchock <klimmy1115@gmail.com>
2024-02-07 08:20:39 -0600

    Finish clearForm, note to maybe integrate Tailwind into this project?

--- Calorie Counter/app.js
+++ Calorie Counter/app.js
@@ -108,4 +108,13 @@ function calculateCalories(e) {
 
 function clearForm() {
     const inputContainers = Array.from(document.querySelectorAll(".input-container"));
+
+    for (let i = 0; i < inputContainers.length; i++) {
+        inputContainers[i].innerHTML = "";
+    }
+
+    budgetNumberInput.value = "";
+    output.innerText = "";
+    output.classList.add("hide");
+    clearButton.addEventListener("click", clearForm);
 }
