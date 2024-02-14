c9cbbf23e6dd6a6f98f43b52c2edb6df459071dd
Ben Klimchock <klimmy1115@gmail.com>
2024-02-14 08:07:44 -0600

    Adding button element to display song title alongside song id

--- Music Player/app.js
+++ Music Player/app.js
@@ -89,5 +89,8 @@ let userData = {
 
 const renderSongs = (array) => {}; 
 const songsHTML = array.map((song) => {
-	return `<li id="song-${song.id}" class="playlist-song"></li>`;
+	return `
+		<li id="song-${song.id}" class="playlist-song"></li>
+		<button class="playlist-song-info"><span class="playlist-song-title">${song.title}</span></button>
+		`;
 });
