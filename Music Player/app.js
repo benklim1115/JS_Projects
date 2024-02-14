b335bc1245592a4982c5fbf06fed65728b3146d5
Ben Klimchock <klimmy1115@gmail.com>
2024-02-14 07:38:23 -0600

    Fixing indentation again >:(

--- Music Player/app.js
+++ Music Player/app.js
@@ -7,84 +7,84 @@ const shuffleButton = document.getElementById("shuffle");
 
 const allSongs = [
 {
-    id: 0,
-    title: "Scratching The Surface",
-    artist: "Kramer",
-    duration: "4:25",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3"
+	id: 0,	
+	title: "Scratching The Surface",
+	artist: "Kramer",
+	duration: "4:25",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3"
 },
 {
-    id: 1,
-    title: "Can't Stay Down",
-    artist: "Art Vandelay",
-    duration: "4:15",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+	id: 1,
+	title: "Can't Stay Down",
+	artist: "Art Vandelay",
+		duration: "4:15",
+src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
 },
 {
-    id: 2,
-    title: "Still Learning",
-    artist: "The Mistro",
-    duration: "3:51",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
+	id: 2,
+	title: "Still Learning",
+	artist: "The Mistro",
+	duration: "3:51",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
 },
 {
-    id: 3,
-    title: "Cruising for a Musing",
-    artist: "Kramer",
-    duration: "3:34",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
-  },
-  {
-    id: 4,
-    title: "Never Not Favored",
-    artist: "Newman",
-    duration: "3:35",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
-  },
-  {
-    id: 5,
-    title: "From the Ground Up",
-    artist: "David Puddy",
-    duration: "3:12",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
-  },
-  {
-    id: 6,
-    title: "Walking on Air",
-    artist: "Bob Saccamano",
-    duration: "3:25",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
-  },
-  {
-    id: 7,
-    title: "Can't Stop Me. Can't Even Slow Me Down.",
-    artist: "Newman",
-    duration: "3:52",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
-  },
-  {
-    id: 8,
-    title: "The Surest Way Out is Through",
-    artist: "Art Vandelay",
-    duration: "3:10",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
-  },
-  {
-    id: 9,
-    title: "Chasing That Feeling",
-    artist: "Mr. Costanza",
-    duration: "2:43",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
-  },
+	id: 3,
+	title: "Cruising for a Musing",
+	artist: "Kramer",
+	duration: "3:34",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
+},
+{
+	id: 4,
+	title: "Never Not Favored",
+	artist: "Newman",
+	duration: "3:35",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
+},
+{
+	id: 5,
+	title: "From the Ground Up",
+	artist: "David Puddy",
+	duration: "3:12",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
+},
+{
+	id: 6,
+	title: "Walking on Air",
+	artist: "Bob Saccamano",
+	duration: "3:25",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
+},
+{
+	id: 7,
+	title: "Can't Stop Me. Can't Even Slow Me Down.",
+	artist: "Newman",
+	duration: "3:52",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
+},
+{
+	id: 8,
+	title: "The Surest Way Out is Through",
+	artist: "Art Vandelay",
+	duration: "3:10",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
+},
+{
+	id: 9,
+	title: "Chasing That Feeling",
+	artist: "Mr. Costanza",
+	duration: "2:43",
+	src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
+},
 ];
 
 //setting up Web Audio API
 const audio = new Audio();
 
 let userData = {
-    songs: [...allSongs],
-    currentSong: null,
-    songCurrentTime: 0
+	songs: [...allSongs],
+	currentSong: null,
+	songCurrentTime: 0
 };
 
 const renderSongs = (array) => {}; 
