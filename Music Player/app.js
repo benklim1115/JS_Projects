2290666cedf1a16706f4c1f22105aa8679958827
Ben Klimchock <klimmy1115@gmail.com>
2024-02-17 20:01:32 -0600

    Delete button to list

--- Music Player/app.js
+++ Music Player/app.js
@@ -95,6 +95,7 @@ const songsHTML = array.map((song) => {
 		<span class="playlist-song-title">${song.title}</span>
 		<span class="playlist-song-artist">${song.artist}</span>
 		<span class="playlist-song-duration">${song.duration}</span>
+		<span class="playlist-song-delete" aria-label="delete"></span>
 		</button>
 		`;
 });
