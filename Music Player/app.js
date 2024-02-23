063a38dcb6e1bbfa68d9fb6a66be7963d2bc06fa
Ben Klimchock <klimmy1115@gmail.com>
2024-02-23 08:05:12 -0600

    Add the playing class and play the song using the play property on the audio object

--- Music Player/app.js
+++ Music Player/app.js
@@ -129,8 +129,15 @@ const playSong = (id) => {
 	const song = userData?.songs.find((song) => song.id === id);
 	audio.src = song.src;
 	audio.title = song.title;
+
 	//set song current time to 0 if we are not on the correct song or its null
 	if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
 		audio.currentTime = 0;
+	} else {
+		audio.currentTime = userData?.songCurrentTime;
 	}
+
+	userData.currentSong = song; 
+	playButton.classList.add("playing");
+	audio.play();
 };
