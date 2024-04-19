0fdc170d738e7b9095de316bcd6b02598ede6bc6
Ben Klimchock <klimmy1115@gmail.com>
2024-04-19 07:40:48 -0500

    Setting both currentSong and songCurrentTime to null and zero since we are switching songs

--- Music Player/app.js
+++ Music Player/app.js
@@ -238,4 +238,6 @@ const setPlayButtonAccessibleText = () => {
 //shuffle songs function
 const shuffle = () => {
 	userData?.songs.sort(() => Math.random() - 0.5);
+	userData.currentSong = null;
+	userData.songCurrentTime = 0;  
 };
